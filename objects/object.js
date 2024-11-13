473c72ef2fcaacdf65874a1ade82da73e70d83f2
Saddam Hossain Azad <saddam987020@gmail.com>
2024-11-14 02:35:45 +0600

    Object - Usage, Literals & Object Literals

--- objects/object.js
+++ objects/object.js
@@ -14,3 +14,21 @@ console.log( person.firstName + " " + person.lastName );
 console.log( person["eyeColor"] ); // "blue" - calling `property` like array index
 
 person.sleep(); // calling methods/functions of the Object
+
+
+// Add New Property
+person.weight = "90 lbs";
+
+console.log( person ); // `weight` property added
+
+
+// Change/update existing property's value
+person.eyeColor = "brown";
+
+console.log( person );
+
+
+// Delete property
+delete person.age;
+
+console.log( person ); // `age` property deleted
