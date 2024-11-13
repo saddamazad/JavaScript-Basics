0e3c60de276df716d5d2eb846b6f081e5e289ed2
Saddam Hossain Azad <saddam987020@gmail.com>
2024-11-14 02:14:12 +0600

    Object - Literals & Object Literals

---
+++ objects/object-literals.js
@@ -0,0 +1,13 @@
+// We already know Literal means fixed value, so Object literal means object with fixed values
+// Object literals are also called object `initializers`, the default values that the object is initialized with
+// Object `property` values can be changed later though
+
+
+// we're setting `fixed values` for the object properties during declaration, so it's an object literal
+const myObj = {name: "Azad", city: "Dhaka"};
+
+
+
+/* since these are fixed value without any variable declaration, so these are literals */
+// "Test String"; // literal
+// 30 * 48; // literal
