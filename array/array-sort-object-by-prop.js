491352bf7b660abe813abcbc94f30b89e7bf2316
Saddam Hossain Azad <saddam987020@gmail.com>
2024-11-16 04:31:26 +0600

    arr.sort() ^ arr.toSorted() - sort array of Objects by the objects `property` value

--- array/array-sort-object-by-prop.js
+++ array/array-sort-object-by-prop.js
@@ -22,8 +22,8 @@ console.log( cars );
 
 
 const cars2 = [
-    { type:"Volvo", year:2016 },
     { type:"Saab", year:2001 },
+    { type:"Volvo", year:2016 },
     { type:"BMW", year:2010 }
 ];
 
@@ -35,3 +35,18 @@ let sortedByYear = cars2.toSorted(function(a, b) {
 console.log( sortedByYear );
 
 console.log( cars2 ); // the original `car2` array is unchanged
+
+
+
+// Comparing `string` properties is a little more complex:
+cars2.sort(function(a, b) {
+    let x = a.type.toLowerCase(); // sorting the array by the objects `type` property
+    let y = b.type.toLowerCase(); // sorting the array by the objects `type` property
+
+    if (x < y) { return -1; } // ASC, reverse the comparison operator to get DESC
+    if (x > y) { return 1; } // ASC, reverse the comparison operator to get DESC
+    
+    return 0;
+});
+
+console.log( cars2 );
