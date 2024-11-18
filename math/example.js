ba5b3e8509eb9d597d33ac87485ec7c573eb1925
Saddam Hossain Azad <saddam987020@gmail.com>
2024-11-19 03:40:29 +0600

    Math functions - round(), floor(), ceil(), trunc(), min() & max(), random(), sign() etc.

---
+++ math/example.js
@@ -0,0 +1,41 @@
+
+// Math.round() function rounds a number to the nearest integer
+
+console.log( Math.round(12.53) ); // 13
+console.log( Math.round(12.37) ); // 12
+
+
+console.log( Math.floor(12.53) ); // 12
+
+console.log( Math.ceil(12.47) ); // 13
+
+
+// Math.trunc() function returns the integer part of a number
+
+console.log( Math.trunc(12.53) ); // 12
+
+
+
+// Math.min() and Math.max() can be used to find the lowest or highest value in a list of `arguments`
+
+console.log( Math.min(8, 150, 30, 20) ); // 8
+
+console.log( Math.max(8, 150, 30, 20) ); // 150
+
+
+
+// Math.random() function returns a `random number` between 0 (inclusive), and 1 (exclusive)
+
+console.log( Math.random() ); // 0.7912306202703236
+console.log( Math.random() ); // 0.5265243202310022
+
+
+
+// Math.sign() function checks if a number is negative, null or positive
+
+console.log( Math.sign(-4) ); // -1, so it's a negative number
+
+console.log( Math.sign(4) ); // 1, so it's a positive number
+
+console.log( Math.sign(0) ); // 0, so it's null
+
