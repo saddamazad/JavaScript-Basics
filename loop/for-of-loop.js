952981d8cfb957ab165484fe4eba1b4fe8b04b86
Saddam Hossain Azad <saddam987020@gmail.com>
2024-11-20 05:11:23 +0600

    Loop through the `properties` of an OBJECT

---
+++ loop/for-of-loop.js
@@ -0,0 +1,19 @@
+// `for of` statement loops through the values of an `iterable object` or an array
+
+/* #### Looping over an Array #### */
+
+const cars = ["BMW", "Volvo", "Mini"];
+
+for (let carItem of cars) {
+    console.log( carItem );
+}
+
+
+/* #### Looping over a String/Iterable Object #### */
+
+let language = "JavaScript"; // we know string is a built-in `iterable object`
+
+for (let letter of language) {
+    console.log( letter );
+}
+
