2ef04efd3819573ce7f577e511a9fcfc7847e877
Saddam Hossain Azad <saddam987020@gmail.com>
2024-11-20 03:05:44 +0600

    Some simple tasks/projects

--- simple-projects/count-vowels-in-sentence.js
+++ simple-projects/count-vowels-in-sentence.js
@@ -3,6 +3,8 @@ const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
 function countVowels(sentence) {
     let count = 0;
 
+    // we know "string" is also an special type of `iterable object`
+    // the Array.from() method converts an iterable object into an `array`, we used this in the (array) chapter
     const letters = Array.from(sentence);
 
     letters.forEach(function(value, index, array) {
