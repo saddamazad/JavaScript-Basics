5e7f97e663b1ea9eb8a078218b51d2004e57ce06
Saddam Hossain Azad <saddam987020@gmail.com>
2024-11-21 03:14:29 +0600

    Difference between NULL and Undefined

---
+++ constructor-and-typeof/null-vs-undefined.js
@@ -0,0 +1,17 @@
+// when a variable(any data type) is set to `NULL`, it's removed from the memory and doesn't exist anymore
+// when a variable(any data type) is set to undefined, the variable is stored in the memory with undefined value
+
+
+// null and undefined have same value(nothing)
+
+console.log( null == undefined ); // true
+
+
+// null and undefined are not same type
+
+console.log( null === undefined ); // false
+
+
+console.log( typeof null ); // object
+
+console.log( typeof undefined ); // undefined
