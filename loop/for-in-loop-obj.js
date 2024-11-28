f9da1035828da8df280d68b2620e717c531331fa
Saddam Hossain Azad <saddam987020@gmail.com>
2024-11-29 02:01:04 +0600

    Loop through the 'properties' of an OBJECT || 'For in' loop on Array

--- loop/for-in-loop-obj.js
+++ loop/for-in-loop-obj.js
@@ -12,3 +12,21 @@ for(let prop in person) {
 
 // this loop method can be used for array as well, but it's not recommended.
 // better use the (for), (for of) or (forEach) methods for arrays
+
+const myObj = {
+    name: "Azad",
+    age: 35,
+    cars: [
+        {name: "Ford", models: ["Fiesta", "Focus", "Mustang"]},
+        {name: "BMW", models: ["320", "X3", "X5"]},
+        {name: "Fiat", models: ["500", "Panda"]}
+    ]
+};
+
+for(let idx in myObj.cars) { // `for in` loop on Array returns the numeric index of each element
+    console.log(myObj.cars[idx].name);
+    
+    for(let i in myObj.cars[idx].models) {
+        console.log(`- ${myObj.cars[idx].models[i]}`);
+    }
+}
